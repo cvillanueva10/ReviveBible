d642c5e21968d6b2b234b52cf3b7e9f8a9ab4023
Chris Villanueva <cvillanueva6@ucmerced.edu>
2019-02-20 22:14:45 -0800

    Initial Commit

---
+++ SpontaneousBibleVerses/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpontaneousBibleVerses
+//
+//  Created by Christopher Villanueva on 2/20/19.
+//  Copyright © 2019 Christopher Villanueva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
