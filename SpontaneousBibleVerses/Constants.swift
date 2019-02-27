262065bc6941e2c615c0b61e7a83f1d3f4809060
Chris Villanueva <cvillanueva6@ucmerced.edu>
2019-02-26 21:43:31 -0800

    Verse Objects and API Call set in place

---
+++ SpontaneousBibleVerses/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  SpontaneousBibleVerses
+//
+//  Created by Christopher Villanueva on 2/24/19.
+//  Copyright © 2019 Christopher Villanueva. All rights reserved.
+//
+
+import Foundation
---
+++ SpontaneousBibleVerses/HTTPClient.swift
@@ -0,0 +1,9 @@
+//
+//  HTTPClient.swift
+//  SpontaneousBibleVerses
+//
+//  Created by Christopher Villanueva on 2/25/19.
+//  Copyright © 2019 Christopher Villanueva. All rights reserved.
+//
+
+import Foundation
