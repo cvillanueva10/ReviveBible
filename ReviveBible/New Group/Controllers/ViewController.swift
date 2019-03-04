8fab25c52efd2c386dbe72f237406757326a1e9e
Chris Villanueva <cvillanueva6@ucmerced.edu>
2019-03-03 21:28:02 -0800

    Home Container Class Strucuted Started

--- ReviveBible/New Group/Controllers/ViewController.swift
+++ ReviveBible/New Group/Controllers/ViewController.swift
@@ -12,7 +12,6 @@ class ViewController: UIViewController {
     
     var verses: [Verse] = []
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
---
+++ ReviveBible/Views/HomeContainerView.swift
@@ -0,0 +1,9 @@
+//
+//  HomeContainerView.swift
+//  ReviveBible
+//
+//  Created by Christopher Villanueva on 3/3/19.
+//  Copyright © 2019 Christopher Villanueva. All rights reserved.
+//
+
+import Foundation
