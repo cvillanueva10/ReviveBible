9a6c73a87de0f8b5c51352fd1ba12bbfc7a24a0b
Chris Villanueva <cvillan0123@gmail.com>
2019-03-18 21:48:08 -0700

    Added home tab bar controller

---
+++ ReviveBible/New Group/Controllers/MainTabBarController.swift
@@ -0,0 +1,9 @@
+//
+//  MainTabBarController.swift
+//  ReviveBible
+//
+//  Created by Chris Villanueva on 3/18/19.
+//  Copyright © 2019 Christopher Villanueva. All rights reserved.
+//
+
+import Foundation
